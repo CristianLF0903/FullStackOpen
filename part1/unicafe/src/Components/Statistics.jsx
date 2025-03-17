d9a8df31a831dcf4a2d71a96bdcce4af8552ef0e
Cristian Londoño Franco <clondonof@unal.edu.co>
2025-03-17 14:08:41 -0500

    Exercise 1.9

--- part1/unicafe/src/Components/Statistics.jsx
+++ part1/unicafe/src/Components/Statistics.jsx
@@ -2,12 +2,18 @@ const Statistics = (props) => {
   return (
     <div>
       <h1>Statistics</h1>
-      <p>good {props.good}</p>
-      <p>neutral {props.neutral}</p>
-      <p>bad {props.bad}</p>
-      <p>all {props.total}</p>
-      <p>average {props.avg}</p>
-      <p>positive {props.percent} %</p>
+      {props.total === 0 ? (
+        <p>No feedback given</p>
+      ) : (
+        <>
+          <p>good {props.good}</p>
+          <p>neutral {props.neutral}</p>
+          <p>bad {props.bad}</p>
+          <p>all {props.total}</p>
+          <p>average {props.avg}</p>
+          <p>positive {props.percent} %</p>
+        </>
+      )}
     </div>
   );
 };
