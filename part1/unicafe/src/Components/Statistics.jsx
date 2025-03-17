00681e0d227cd997419d21e2c71355964d8eb50f
Cristian Londoño Franco <clondonof@unal.edu.co>
2025-03-17 14:23:48 -0500

    Exercise 1.11

--- part1/unicafe/src/Components/Statistics.jsx
+++ part1/unicafe/src/Components/Statistics.jsx
@@ -7,14 +7,16 @@ const Statistics = (props) => {
       {props.total === 0 ? (
         <p>No feedback given</p>
       ) : (
-        <>
-          <StatisticLine text="good" value={props.good} />
-          <StatisticLine text="neutral" value={props.neutral} />
-          <StatisticLine text="bad" value={props.bad} />
-          <StatisticLine text="total" value={props.total} />
-          <StatisticLine text="average" value={props.avg} />
-          <StatisticLine text="positive" value={props.percent + " %"} />
-        </>
+        <table>
+          <tbody>
+            <StatisticLine text="good" value={props.good} />
+            <StatisticLine text="neutral" value={props.neutral} />
+            <StatisticLine text="bad" value={props.bad} />
+            <StatisticLine text="total" value={props.total} />
+            <StatisticLine text="average" value={props.avg} />
+            <StatisticLine text="positive" value={props.percent + " %"} />
+          </tbody>
+        </table>
       )}
     </div>
   );
