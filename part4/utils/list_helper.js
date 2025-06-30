a0da679f4d9a66f61706003cec7a2e1e72e5be74
Cristian Londoño Franco <clondonof@unal.edu.co>
2025-06-30 12:33:49 -0500

    Excercises 4.3 - 4.6

---
+++ part4/utils/list_helper.js
@@ -0,0 +1,18 @@
+const dummy = (blogs) => {
+  return 1
+}
+
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, blog) => sum + blog.likes, 0)
+}
+
+const mostLikes = (blogs) => {
+  if (blogs.length === 0) return null
+  return blogs.reduce((max, blog) => (blog.likes > max.likes ? blog : max), blogs[0])
+}
+
+module.exports = {
+  dummy,
+  totalLikes,
+  mostLikes
+}
